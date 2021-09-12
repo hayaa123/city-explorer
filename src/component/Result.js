f940133ee37e73189c8d9ffc40395c54e56bdb7a
hayaa1234 <hayalawansah@gmail.com>
2021-09-12 19:24:08 +0300

    lab-06

--- src/component/Result.js
+++ src/component/Result.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react'
-import { Card } from 'react-bootstrap' 
+import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 export class Result extends Component {
-    render() {
-        return (
-            <>
-              
-
-    <Card border="primary" style={{ width: '18rem' ,marginLeft:150 ,position:'absolute' ,left:700, top:300}}>
-             <Card.Header>{this.props.city_name}</Card.Header>
-            <Card.Body>
-      <Card.Text>
-              <p>latitude :{this.props.lat}</p>  
+  render() {
+    return (
+      <>
+        <Card
+          border="primary"
+          style={{
+            width: "18rem",
+            marginLeft: 150,
+            position: "absolute",
+            left: 700,
+            top: 300,
+          }}
+        >
+          <Card.Header>{this.props.city_name}</Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <p>latitude :{this.props.lat}</p>
               <p>longtitude :{this.props.lon}</p>
-      </Card.Text>
-    </Card.Body>
-  </Card>  
-            </>
-        )
-    }
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </>
+    );
+  }
 }
 
-export default Result
+export default Result;
